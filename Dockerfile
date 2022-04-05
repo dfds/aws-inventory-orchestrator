6308259893f9e9efafa6358451fceaedfc6265b5
Peter West <peterjwest@hotmail.com>
2022-04-05 15:55:03 +0100

    Initial cronjob setup.

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,15 @@ FROM golang:1.18-alpine AS builder
 
 RUN apk add --update --no-cache git
 
-ADD ./src /src
+#ADD ./src /src
+WORKDIR /app
+COPY ./src/go.mod ./
+COPY ./src/go.sum ./
+RUN go mod download
 
-WORKDIR /src
-
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO11MODULE=on go build -mod=mod -a -o /main .
+COPY ./src/*.go ./
+RUN go build -o /main
+#RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO11MODULE=on go build -mod=mod -a -o /main .
 
 FROM alpine:latest
 COPY --from=builder /main /inventory-orchestrator
--- src/main.go
+++ src/main.go
@@ -11,22 +11,19 @@ import (
 )
 
 func main() {
-	for {
-		sess, err := session.NewSession(&aws.Config{Region: aws.String("eu-central-1")})
-		if err != nil {
-			log.Fatalln(err)
-		}
 
-		stsCli := sts.New(sess)
-
-		fmt.Println("I love Go!!")
-
-		resp, err := stsCli.GetCallerIdentity(nil)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		fmt.Println(resp.String())
-		time.Sleep(time.Second * 30)
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("eu-central-1")})
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	stsCli := sts.New(sess)
+
+	resp, err := stsCli.GetCallerIdentity(nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(resp.String())
+	time.Sleep(time.Second * 30)
 }
