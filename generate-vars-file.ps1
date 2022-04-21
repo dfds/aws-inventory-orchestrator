84aafdb97c791dc646cae685c193fe168a5fcc8e
abstrask <rask.misc@gmail.com>
2022-04-21 14:54:49 +0200

    Cleanup and rename variables for non-infrastructure

--- generate-vars-file.ps1
+++ generate-vars-file.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 $VarsFile = "./k8s/vars.env"
 
 "BILLING_ACCOUNT_ID=$(Get-Item "Env:/BILLING_ACCOUNT_ID" | Select -Expand Value)
-SECURITY_ACCOUNT_ID=$(Get-Item "Env:/RUNNER_ACCOUNT_ID" | Select -Expand Value)
+SECURITY_ACCOUNT_ID=$(Get-Item "Env:/SECURITY_ACCOUNT_ID" | Select -Expand Value)
 ORCHESTRATOR_ROLE=$(Get-Item "Env:/ORCHESTRATOR_ROLE" | Select -Expand Value)
 RUNNER_ROLE=$(Get-Item "Env:/RUNNER_ROLE" | Select -Expand Value)
 BUCKET_NAME=$(Get-Item "Env:/BUCKET_NAME" | Select -Expand Value)
