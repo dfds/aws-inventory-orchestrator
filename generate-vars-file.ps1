2e71b5a5da504539374d9fbf121a1934a2b7549c
abstrask <raras@dfds.com>
2022-04-22 14:01:28 +0200

    Remove unused OIDC_PROVIDER var

--- generate-vars-file.ps1
+++ generate-vars-file.ps1
@@ -7,5 +7,4 @@ ORCHESTRATOR_ROLE=$(Get-Item "Env:/ORCHESTRATOR_ROLE" | Select -Expand Value)
 RUNNER_ROLE=$(Get-Item "Env:/RUNNER_ROLE" | Select -Expand Value)
 BUCKET_NAME=$(Get-Item "Env:/BUCKET_NAME" | Select -Expand Value)
 CRON_SCHEDULE=$(Get-Item "Env:/CRON_SCHEDULE" | Select -Expand Value)
-INCLUDE_ACCOUNTS=$(Get-Item "Env:/INCLUDE_ACCOUNTS" | Select -Expand Value)
-OIDC_PROVIDER=$(Get-Item "Env:/OIDC_PROVIDER" | Select -Expand Value)" | Out-File $VarsFile
+INCLUDE_ACCOUNTS=$(Get-Item "Env:/INCLUDE_ACCOUNTS" | Select -Expand Value)" | Out-File $VarsFile
